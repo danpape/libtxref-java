7e4bcac7b1312142214f04fb5ff3b7f895025b79
Daniel X. Pape <dpape@dpape.com>
2021-03-04 08:01:29 -0800

    update examples and README

--- src/main/java/design/contract/example/TxrefEncodingExample.java
+++ src/main/java/design/contract/example/TxrefEncodingExample.java
@@ -4,25 +4,56 @@ import design.contract.txref.Txref;
 
 public class TxrefEncodingExample {
 
-    private static void createStandardTxref() {
+    /**
+     * Create a txref for a mainnet transaction, with only a blockHeight and transactionPosition
+     */
+    private static void createStandardMainnetTxref() {
         int blockHeight = 466793;
         int transactionPosition = 2205;
 
-        // create a simple txref for a testnet transaction, with only a
-        // blockHeight and transactionPosition
+        String txref = Txref.encode(blockHeight, transactionPosition);
+
+        System.out.println(txref);
+        // prints "tx1:rjk0-uqay-z9l7-m9m"
+    }
+
+    /**
+     * Create a txref for a testnet transaction, with only a blockHeight and transactionPosition
+     */
+    private static void createStandardTestnetTxref() {
+        int blockHeight = 466793;
+        int transactionPosition = 2205;
+
         String txref = Txref.encodeTestnet(blockHeight, transactionPosition);
 
         System.out.println(txref);
         // prints "txtest1:xjk0-uqay-zghl-p89"
     }
 
-    private static void createExtendedTxref() {
+    /**
+     * Create an extended txref for a mainnet transaction, with a blockHeight and
+     * transactionPosition and a specific txoIndex of 3
+     */
+    private static void createExtendedMainnetTxref() {
         int blockHeight = 466793;
         int transactionPosition = 2205;
         int txoIndex = 3;
 
-        // create an extended txref for a testnet transaction, with a
-        // blockHeight and transactionPosition and a specific txoIndex of 3
+        String txref = Txref.encode(blockHeight, transactionPosition, txoIndex);
+
+        System.out.println(txref);
+        // prints "tx1:yjk0-uqay-zrqq-34y3-06"
+    }
+
+    /**
+     * Create an extended txref for a testnet transaction, with a blockHeight and
+     * transactionPosition and a specific txoIndex of 3
+     */
+    private static void createExtendedTestnetTxref() {
+        int blockHeight = 466793;
+        int transactionPosition = 2205;
+        int txoIndex = 3;
+
         String txref = Txref.encodeTestnet(blockHeight, transactionPosition, txoIndex);
 
         System.out.println(txref);
@@ -31,8 +62,10 @@ public class TxrefEncodingExample {
 
     public static void main(String[] args) {
 
-        createStandardTxref();
-        createExtendedTxref();
+        createStandardMainnetTxref();
+        createStandardTestnetTxref();
+        createExtendedMainnetTxref();
+        createExtendedTestnetTxref();
 
     }
 
